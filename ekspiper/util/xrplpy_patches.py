56dd440d8a33de1ec754e48cc138b9110529c40b
Amin <amind1@vt.edu>
2023-04-24 12:55:40 -0700

    removed uneccesary uncache function

--- ekspiper/util/xrplpy_patches.py
+++ ekspiper/util/xrplpy_patches.py
@@ -6,37 +6,6 @@ from xrpl.models.utils import require_kwargs_on_init
 
 from xrpl.asyncio.clients import Client, XRPLRequestFailureException
 
-import sys
-
-
-def uncache(exclude = []):
-    """Remove package modules from cache except excluded ones.
-    On next import they will be reloaded.
-
-    Args:
-        exclude (iter<str>): Sequence of module paths.
-    """
-    pkgs = []
-    for mod in exclude:
-        pkg = mod.split('.', 1)[0]
-        pkgs.append(pkg)
-
-    to_uncache = []
-    for mod in sys.modules:
-        if mod in exclude:
-            continue
-
-        if mod in pkgs:
-            to_uncache.append(mod)
-            continue
-
-        for pkg in pkgs:
-            if mod.startswith(pkg + '.'):
-                to_uncache.append(mod)
-                break
-
-    for mod in to_uncache:
-        del sys.modules[mod]
 
 async def get_latest_validated_ledger_sequence(client: Client) -> int:
     """
