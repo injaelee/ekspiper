01546a45ac82bb70248746125264fb67e28cc409
Amin <amind1@vt.edu>
2023-05-04 13:59:49 -0700

    changed debug endpoints to use clio

--- ekspiper/util/endpoints.py
+++ ekspiper/util/endpoints.py
@@ -1,11 +1,11 @@
 endpoints = {
     "mainnet": "https://s2-clio.ripple.com:51234/",
     "testnet": "https://s.altnet.rippletest.net:51234/",
-    "debug": "https://s.altnet.rippletest.net:51234/"
+    "debug": "https://s2-clio.ripple.com:51234/"
 }
 
 wss_endpoints = {
     "mainnet": "wss://s2-clio.ripple.com",
     "testnet": "wss://s.altnet.rippletest.net",
-    "debug": "wss://s.altnet.rippletest.net",
+    "debug": "wss://s2-clio.ripple.com",
 }
